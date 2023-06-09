22471d01822a17ed7913ba6ada628ce067347720
LOUDINI SOUAD <souad.llddnn@gmail.com>
2023-06-09 20:52:34 +0100

    BeautfulSoup code updated

--- Scraper1.py
+++ Scraper1.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(response.text, 'html.parser')
-response = requests.get('https://www.woolworths.com.au/')
+url = 'https://www.woolworths.com.au/'
 
-if response.status_code == 200:
-    table = soup.findAll("table",attrs={"class":"infobox_v2 noarchive"})
-    prices = soup.find_all('span', class_='price-dollars')  
-  
+response = requests.get(url).text
+
+soup = BeautifulSoup(response, 'html.parser')
+
+price = soup.find_all('div', class_="product-tile-price")
+print(price)
+
